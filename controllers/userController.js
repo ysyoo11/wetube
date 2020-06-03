c74c14d624ac4f63a31e12f15276f23ae732e3f6
ysyoo11 <62553092+ysyoo11@users.noreply.github.com>
2020-06-03 17:25:31 +0900

    Layouts with Pug

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
-export const join = (req, res) => res.send("Join");
-export const login = (req, res) => res.send("Login");
-export const logout = (req, res) => res.send("Logout");
-export const users = (req, res) => res.send("Users");
-export const userDetail = (req, res) => res.send("User detail");
-export const editProfile = (req, res) => res.send("Edit profile");
-export const changePassword = (req, res) => res.send("Change password");
+export const join = (req, res) => res.render("join");
+export const login = (req, res) => res.render("login");
+export const logout = (req, res) => res.render("logout");
+export const users = (req, res) => res.render("users");
+export const userDetail = (req, res) => res.render("userDetail");
+export const editProfile = (req, res) => res.render("editProfile");
+export const changePassword = (req, res) => res.render("changePassword");
