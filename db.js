c81736d9004fcb4703f1555b59e700da4cb5ee46
ysyoo11 <62553092+ysyoo11@users.noreply.github.com>
2020-06-04 23:31:01 +0900

    Home Controller Part 1
    Testing on fake database

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+  {
+    id: 546897,
+    title: "The Maldives",
+    description: "Honeymoon trip",
+    views: 569,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 10302,
+      name: "Yeonsuk",
+      email: "yysuk@gmail.com",
+    },
+  },
+  {
+    id: 3456,
+    title: "Jieun",
+    description: "My wife",
+    views: 569,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 10302,
+      name: "Yeonsuk",
+      email: "yysuk@gmail.com",
+    },
+  },
+  {
+    id: 1579,
+    title: "One Punch Man",
+    description: "Saitama sensei is so cool",
+    views: 569,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 10302,
+      name: "Yeonsuk",
+      email: "yysuk@gmail.com",
+    },
+  },
+];
