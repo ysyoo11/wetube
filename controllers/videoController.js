1f7742d1f082989697cabec6160e90ba390394a5
ysyoo11 <62553092+ysyoo11@users.noreply.github.com>
2020-06-05 14:16:30 +0900

    Edited other controllers
    - Controllers of upload and logout
    - Tested solved /:id problem with fake database
    - Added "required=true" on the pages with forms

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
 import { videos } from "../db";
+import routes from "../routes";
 export const home = (req, res) => {
   res.render("home", { pageTitle: "Home", videos });
 };
@@ -9,8 +10,18 @@ export const search = (req, res) => {
   res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
-export const upload = (req, res) =>
+export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+
+export const postUpload = (req, res) => {
+  const {
+    body: { file, title, description },
+  } = req;
+  // ToDo: Upload and save video
+  res.redirect(routes.videoDetail(546897));
+  // After uploading, new id will be assigned to the video.
+  // The user will be redirected to the "videoDetail" page corresponds to the newly uploaded video.
+};
 
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
